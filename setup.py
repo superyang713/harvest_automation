1919680f35890b435d854c4f05cf5e3798948a43
Yang Dai <yang.dai2020@gmail.com>
2021-08-10 16:07:41 -0400

    get ready for publishing to pypi

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setuptools.setup(
     author_email="yang.dai2020@gmail.com",
     description="An automation tool to speed up the process of updating entries in harvest",
     long_description=long_description,
+    download_url = "https://github.com/superyang713/harvest_automation/archive/refs/tags/v0.0.1.tar.gz", 
     url="https://github.com/superyang713/harvest_automation/blob/main/README.md",
     packages=setuptools.find_packages(),
     classifiers=[
@@ -25,3 +26,4 @@ setuptools.setup(
         "selenium",
     ],
 )
+

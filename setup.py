46260c8e293361bddb3fe230d3c0c7bfb5242fd0
Yang Dai <yang.dai2020@gmail.com>
2021-08-10 17:09:46 -0400

    indicate the long description as markdown

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setuptools.setup(
     author_email="yang.dai2020@gmail.com",
     description="An automation tool to speed up the process of updating entries in harvest",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     download_url = "https://github.com/superyang713/harvest_automation/archive/refs/tags/v0.0.2.tar.gz", 
     url="https://github.com/superyang713/harvest_automation/blob/main/README.md",
     packages=setuptools.find_packages(),
